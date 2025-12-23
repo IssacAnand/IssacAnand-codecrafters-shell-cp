0ca2a41f0977ef51cecea5088a0994cf6ea78bfb
Issac <issacdread@hotmail.com>
2025-12-23 11:58:43 +0800

    Receieve command

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <string>
 
+using std::string;
+using std::cout;
+using std::endl;
+using std::cin;
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+  cout << "$ ";
+
+  string command;
+  std::getline(cin,command);
+  cout << command << ": command not found" <<endl;
 }
