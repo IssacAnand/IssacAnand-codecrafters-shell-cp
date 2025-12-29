c29ba83f4532013503b31de8ebedf70dd92a0ab3
Issac <issacdread@hotmail.com>
2025-12-29 14:56:00 +0800

    Revised Changes

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,9 @@ using std::cout;
 using std::endl;
 using std::cin;
 namespace fs =std::filesystem;
-// shd return string instead
+
+
+
 bool check_path(string full_path, string argument) {
   // check functionality
   bool condition{true};
@@ -17,27 +19,23 @@ bool check_path(string full_path, string argument) {
   while(!full_path.empty()){
     // break the string and test here
     size_t colon_pos = full_path.find(':');
+    string directory;
 
     if(colon_pos != string::npos){ //npos is a way of saying not found, since cant return 0 as 0 is an index
-      string directory = full_path.substr(0,colon_pos);
+       directory = full_path.substr(0,colon_pos);
       string target_file = directory + '/' + argument;
-      if(fs::exists(target_file)){
-        cout << argument << " is " << target_file<<endl;
-        return true;
-
-      }
+      full_path.erase(0,colon_pos+1);
+    }
       else{
         //check the last path
-        string last_target_file = full_path + '/' + argument;
-        if(fs::exists(last_target_file)){
-        cout << argument << " is " << target_file<<endl;
+        directory= full_path;
+        full_path.clear();
+      }
+        string target_file = full_path + '/' + argument;
+        if(fs::exists(target_file)){
+        cout << argument << " is " << target_file <<endl;
         return true;
         }
-       
-      }
-    }
-      // To break the loop
-      full_path.clear();
   }
  return false;
 }
@@ -54,10 +52,10 @@ for(string b: BUILT_IN_TYPES){
   // cout << raw_path;
   if (raw_path != nullptr) {
     string current_path = raw_path;
-    check_path(current_path, cmd);
+    return check_path(current_path, cmd); //return true or false
   
   }
-  return false;
+  
 }
 
 int main() {
