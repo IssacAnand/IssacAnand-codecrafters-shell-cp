01066637974a7156fcb6fa40cd9714ff46ce1031
Issac <issacdread@hotmail.com>
2025-12-29 15:26:50 +0800

    Fixed cases

--- src/main.cpp
+++ src/main.cpp
@@ -90,7 +90,7 @@ while (condition){
     else if(command.substr(0,4)=="type"){
       string argument = command.erase(0,5);
       string result = is_in_builtin(argument);
-      if ( result =="echo"|| result == "exit"){
+      if ( result =="echo"|| result == "exit" || result =="type"){
         cout << result << " is a shell builtin"<<endl;
       }
       else if (result != ""){
